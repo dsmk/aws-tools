3a2ad155f99916066e78eedb859a1f54c10beee2
Anton Kachurin <kachurinanton@gmail.com>
2019-06-13 12:40:26 -0400

    Create credentials file if doesn't exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,9 @@ echo "If you need to authenticate for a regular AWS account: aws configure"
 echo ""
 echo "If you need to authenticate using Shibboleth: shib-auth"
 
+if [ ! -f /root/.aws/credentials ]; then
+    mkdir -p /root/.aws
+    touch /root/.aws/credentials
+fi
+
 exec "$@"
